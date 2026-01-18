0ed03cd53d72950d1cb4ceb4b4f26989241652d7
Frytoslav <filipbujakowski1@gmail.com>
2026-01-18 22:34:48 +0100

    refactor: update static file pathing and enhance main layout styles

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Request
-from pathlib import Path
-from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from routers import books
 from contextlib import asynccontextmanager
-from database import Base, engine
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.database import Base, engine
+from app.init_db import init_db
+from app.routers import books
 import os
 
 Base.metadata.create_all(bind=engine)
@@ -14,7 +15,7 @@ app = FastAPI(title="LibraryLite")
 BASE_DIR = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 static_dir = os.path.join(BASE_DIR, "static")
-app.mount("/static", StaticFiles(directory=static_dir), name="static")
+app.mount("/static", StaticFiles(directory=str(BASE_DIR / "static")), name="static")
 
 app.include_router(books.router)
 
